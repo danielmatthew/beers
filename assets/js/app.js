7e9a6146fa7f87bc65e01e63857113f15dc5d506
Daniel <hello@danmatthew.co.uk>
2013-04-16 08:21:14 +0100

    Refactored pagination
    Use documentFragment to insert all new items at once rather than doing
    so within for loop. Performance ftw, right?!

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,7 +11,7 @@
 
 	function addEventListeners() {
 		document.getElementById("add").addEventListener('click', addBeer, false);
-		document.getElementById("paginate").addEventListener('click', drawList, false);		
+		document.getElementById("paginate").addEventListener('click', paginate, false);		
 	}
 
 
@@ -41,30 +41,7 @@
 		xhr.send(new FormData(form));	
 	}
 
-// Renders pagination results
-function drawList() {
-	if (event.preventDefault) {
-		event.preventDefault();
-	}
 
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4) {
-			if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
-				console.log(xhr.responseText);
-				var response = JSON.parse(xhr.responseText);
-				drawText(response, 5);
-			}
-			else {
-				console.log("drawList went wrong: " + xhr.status);
-			}
-		}
-	}
-	xhr.open("get", "test.php?offset=" + paginationOffset, true);
-	paginationOffset += 5;
-	xhr.send(null);
-	document.body.scrollByLines(5);
-}
 
 // Draws number of list items with specified content
 function drawText(content, num) {
@@ -95,34 +72,88 @@ function drawText(content, num) {
 	// beers.appendChild(fragment);
 }
 
-// TODO: Draws list item
-function drawListItem(content) {
-	var thumbnail = null;
-	var name = null;
+	// Draws new item at top of list
+	function drawListItem(content) {
+		var thumbnail = null;
+		var name = null;
 
-	var fragment = document.createDocumentFragment();
+		var fragment = document.createDocumentFragment();
 
-	//Create thumbnail
-	thumbnail = document.createElement('img');
-	thumbnail.setAttribute('src', 'assets/img/thumb.png');
-
-
-	//Create beer name
-	name = document.createElement('h1');
-	name.appendChild(document.createTextNode(content));
+		//Create thumbnail
+		thumbnail = document.createElement('img');
+		thumbnail.setAttribute('src', 'assets/img/thumb.png');
 
 
-	//Create list item
-	li = document.createElement('li');
-	li.className = 'fade';
-	li.appendChild(thumbnail);
-	li.appendChild(name);
-	fragment.appendChild(li);
+		//Create beer name
+		name = document.createElement('h1');
+		name.appendChild(document.createTextNode(content));
 
-	//Add to top of list
-	//document.getElementById("beers").appendChild(fragment);
-	document.getElementById("beers").insertBefore(fragment, beers.firstChild);
-}
+
+		//Create list item
+		li = document.createElement('li');
+		li.className = 'fade';
+		li.appendChild(thumbnail);
+		li.appendChild(name);
+		fragment.appendChild(li);
+
+		//Add to top of list
+		//document.getElementById("beers").appendChild(fragment);
+		document.getElementById("beers").insertBefore(fragment, beers.firstChild);
+	}
+
+	// Draws five more items at bottom of list
+	function drawPaginationItems(content, num) {
+		var thumbnail = null;
+		var name = null;
+
+		var fragment = document.createDocumentFragment();
+
+		for (var i = 0; i < content.length; i++) {
+			//Create thumbnail
+			thumbnail = document.createElement('img');
+			thumbnail.setAttribute('src', 'assets/img/thumb.png');
+
+
+			//Create beer name
+			name = document.createElement('h1');
+			name.appendChild(document.createTextNode(content[i].name));
+
+
+			//Create list item
+			li = document.createElement('li');
+			li.className = 'fade';
+			li.appendChild(thumbnail);
+			li.appendChild(name);
+			fragment.appendChild(li);
+		}
+
+		//Add to bottom of list
+		document.getElementById("beers").appendChild(fragment);		
+	}
+
+	// Renders pagination results
+	function paginate() {
+		if (event.preventDefault) {
+			event.preventDefault();
+		}
+
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4) {
+				if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
+					console.log(xhr.responseText);
+					var response = JSON.parse(xhr.responseText);
+					drawPaginationItems(response, 5);
+				}
+				else {
+					console.log("drawList went wrong: " + xhr.status);
+				}
+			}
+		}
+		xhr.open("get", "test.php?offset=" + paginationOffset, true);
+		paginationOffset += 5;
+		xhr.send(null);
+	}
 
 // Renders error message for user
 function drawErrorMessage(msg) {
